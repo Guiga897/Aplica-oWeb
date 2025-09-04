3a72c0feb703382138e84956472cbdc4a793025a
Guiga897 <guifavero10@gmail.com>
2025-09-03 21:56:43 -0300

    falta coisa

--- aweb/aula_03/to-do-list/src/main/java/br/com/aweb/to_do_list/models/Todo.java
+++ aweb/aula_03/to-do-list/src/main/java/br/com/aweb/to_do_list/models/Todo.java
@@ -8,6 +8,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,13 +21,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class Todo {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min=3, max=100)
+    @NotBlank
     @Column(length = 100,nullable = false)
     private String title;
     @Column(nullable = false)
-    private LocalDateTime  createdAt;
+    private LocalDateTime  createdAt = LocalDateTime.now();
+    @NotNull
+    @FutureOrPresent
     @Column(nullable = false)
     private LocalDate deadline;
     @Column(nullable = true)
